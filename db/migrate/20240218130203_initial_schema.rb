d9f9ec72c0df5949a46189cdb117ad0689e5643d
Murdho Savila <murdho@murdho.com>
2024-02-18 16:35:42 +0200

    Create initial schema

---
+++ db/migrate/20240218130203_initial_schema.rb
@@ -0,0 +1,61 @@
+class InitialSchema < ActiveRecord::Migration[7.2]
+  def change
+    create_table :statement_formats do |t|
+      t.string :label, null: false, index: { unique: true }
+      t.string :encoding, null: false
+      t.string :col_sep, null: false
+      t.string :date_fmt, null: false
+      t.string :date_col, null: false
+      t.string :amount_col, null: false
+      t.string :currency_col, null: false
+      t.string :party_col
+      t.string :description_col
+
+      t.timestamps
+    end
+
+    create_table :statements do |t|
+      t.string :filename, null: false, index: { unique: true }
+      t.references :statement_format, null: false, foreign_key: true
+
+      t.timestamps
+    end
+
+    create_table :categories do |t|
+      t.string :name, null: false
+      t.string :type, null: false
+
+      t.timestamps
+    end
+
+    create_table :rules do |t|
+      t.date :date
+      t.string :amount
+      t.string :currency
+      t.string :party
+      t.string :description
+      t.string :note
+      t.string :strictness, null: false, default: "lenient"
+      t.references :category, null: false, foreign_key: true, index: true
+
+      t.timestamps
+    end
+
+    create_table :bank_transactions do |t|
+      t.date :date
+      t.string :amount, null: false
+      t.string :currency, null: false
+      t.string :party
+      t.string :description
+      t.references :statement, null: false, foreign_key: true
+      t.references :category, foreign_key: true, index: true
+      t.references :rule, foreign_key: true, index: true
+
+      t.timestamps
+    end
+
+    add_check_constraint :categories, "type IN ('required', 'beneficial', 'optional', 'other', 'income')"
+    add_check_constraint :rules, "strictness IN ('lenient', 'strict')"
+    add_check_constraint :rules, "coalesce(date, amount, currency, party, description) IS NOT NULL"
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,83 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.2].define(version: 2024_02_18_130203) do
+  create_table "bank_transactions", force: :cascade do |t|
+    t.date "date"
+    t.string "amount", null: false
+    t.string "currency", null: false
+    t.string "party"
+    t.string "description"
+    t.integer "statement_id", null: false
+    t.integer "category_id"
+    t.integer "rule_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_bank_transactions_on_category_id"
+    t.index ["rule_id"], name: "index_bank_transactions_on_rule_id"
+    t.index ["statement_id"], name: "index_bank_transactions_on_statement_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "type", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.check_constraint "type IN ('required', 'beneficial', 'optional', 'other', 'income')"
+  end
+
+  create_table "rules", force: :cascade do |t|
+    t.date "date"
+    t.string "amount"
+    t.string "currency"
+    t.string "party"
+    t.string "description"
+    t.string "note"
+    t.string "strictness", default: "lenient", null: false
+    t.integer "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_rules_on_category_id"
+    t.check_constraint "coalesce(date, amount, currency, party, description) IS NOT NULL"
+    t.check_constraint "strictness IN ('lenient', 'strict')"
+  end
+
+  create_table "statement_formats", force: :cascade do |t|
+    t.string "label", null: false
+    t.string "encoding", null: false
+    t.string "col_sep", null: false
+    t.string "date_fmt", null: false
+    t.string "date_col", null: false
+    t.string "amount_col", null: false
+    t.string "currency_col", null: false
+    t.string "party_col"
+    t.string "description_col"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["label"], name: "index_statement_formats_on_label", unique: true
+  end
+
+  create_table "statements", force: :cascade do |t|
+    t.string "filename", null: false
+    t.integer "statement_format_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["filename"], name: "index_statements_on_filename", unique: true
+    t.index ["statement_format_id"], name: "index_statements_on_statement_format_id"
+  end
+
+  add_foreign_key "bank_transactions", "categories"
+  add_foreign_key "bank_transactions", "rules"
+  add_foreign_key "bank_transactions", "statements"
+  add_foreign_key "rules", "categories"
+  add_foreign_key "statements", "statement_formats"
+end
