b4394b48041808eb0dbb067e2970cf16202b6ef6
Murdho Savila <murdho@murdho.com>
2024-03-10 18:40:19 +0200

    Refactor overview into helpers, partials

--- app/helpers/overview_helper.rb
+++ app/helpers/overview_helper.rb
@@ -1,13 +1,21 @@
 module OverviewHelper
-  def money(money)
+  def month_tag(month)
+    Date.parse("#{month}-01").strftime("%B %Y")
+  end
+
+  def money_tag(money)
     humanized_money money, no_cents_if_whole: false
   end
 
-  def money_color_class(amount_cents, positive_class, negative_class, zero_class)
+  def money_color(amount_cents, positive, negative, zero)
     case
-    when amount_cents > 0 then positive_class
-    when amount_cents < 0 then negative_class
-    else zero_class
+    when amount_cents > 0 then positive
+    when amount_cents < 0 then negative
+    else zero
     end
+  end
+
+  def link_to_select_category(name, id)
+    link_to (name || "—"), { selected_category_id: id }, "data-turbo-action": :replace
   end
 end
