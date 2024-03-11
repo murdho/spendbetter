9f7dd264f7ffab6a6e7668dbba5c3002b1c3d372
Murdho Savila <murdho@murdho.com>
2024-03-11 07:40:50 +0200

    Explore overview UI ideas

--- app/controllers/overview_controller.rb
+++ app/controllers/overview_controller.rb
@@ -4,6 +4,7 @@ class OverviewController < ApplicationController
   def index
     @bank_transactions_for_summary = BankTransaction.for_summary
     @bank_transactions_for_details = BankTransaction.for_details(selected_category_id)
+    @categories_for_select = Category.all
   end
 
   def selected_category_id
