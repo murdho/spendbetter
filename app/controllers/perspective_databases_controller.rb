e03aa7aa812a8b8bdf925d690366a3c5c32447a3
Murdho Savila <murdho@murdho.com>
2025-05-02 14:06:37 +0300

    Fix perspective_database etag handling

--- app/controllers/perspective_databases_controller.rb
+++ app/controllers/perspective_databases_controller.rb
@@ -2,8 +2,7 @@ class PerspectiveDatabasesController < ApplicationController
   def show
     Perspective.database do
       it.refresh! unless it.exists?
-      fresh_when etag: it.updated_at
-      send_file it.path if stale?
+      send_file it.path if stale? etag: it.updated_at
     end
   end
 end
